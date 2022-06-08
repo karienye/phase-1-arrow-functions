99030e656c22722ad78fec58139a8a9c8aed4030
leah karienye <karienyeleah@gmail.com>
2022-06-08 09:45:42 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+/*let a = 2000, b = 100;
+const divide = (a / b ); => 
+{square (a * a);
+
+}*/
+const divide = (a = 2000, b = 100) => {
+   
+    return a / b;
+  }
+  const num = 6;
+  const square =  (num.map(y => y ** 2) )
+
+  let add = (x) => x + x;
+
+console.log(add(10)); 
+/*
+  const add = (x) => {
+    console.log(`Adding ${x}`);
+    return x + x;
+  }
+  add(4);
+
+  */
